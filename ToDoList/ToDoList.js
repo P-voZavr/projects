979852e1b7605c2cd3b5e6b08acedf048aece856
P_voZavr <165915639+P-voZavr@users.noreply.github.com>
2024-12-17 19:33:57 +0200

    Todo update

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -16,9 +16,36 @@ function del(par) {
 function refresh() {
   let list = "";
   for (let s = 0; s < ToDo.length; s++) {
-    list += `<div class="tododiv"><p class="todo">${s + 1}. ${
-      ToDo[s]
-    }</p> <button class="delButton" onclick="del(${s})">delete</button></div>`;
+    if (ToDo[s].includes("<")) {
+      list += `<div class="tododiv"> ${ToDo[s]}
+       <button class="delButton" onclick="del(${s})">delete</button>
+       <button class="renameButton" onclick="rename(${s})">rename</button></div>`;
+    } else {
+      list += `<div class="tododiv"><p class="todo">${s + 1}. ${
+        ToDo[s]
+      }</p> <button class="delButton" onclick="del(${s})">delete</button>
+    <button class="renameButton" onclick="rename(${s})">rename</button></div>`;
+    }
   }
   document.querySelector(".td").innerHTML = list;
+}
+
+function rename(par) {
+  ToDo[
+    par
+  ] = `<input class="renameInpt" onkeydown='rename2(${par})' placeholder="Rename" >`;
+  refresh();
+}
+
+function rename2(par) {
+  if (event.key === "Enter") {
+    ToDo[par] = document.querySelector(".renameInpt").value;
+    refresh();
+  }
+}
+
+function dopact() {
+  if (event.key === "Enter") {
+    addtodo();
+  }
 }
